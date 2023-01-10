446ebb9b40a2713baf898ab912edc2fb891ffbb5
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2023-01-09 22:42:54 -0500

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import NotFound from './Pages/NotFound';
