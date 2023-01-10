c6420b9d0aff3be8c75027ac94cbfb1fb1535da0
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2023-01-09 23:18:25 -0500

    Updates

--- src/Pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="section" id="not-found">
-      <h1>Uh oh, looks like you took a wrong turn somewhere.</h1>
-    </div>
-  );
-};
-
-export default NotFound;
