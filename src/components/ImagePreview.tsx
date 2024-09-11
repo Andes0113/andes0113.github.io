4c446c72bae10956062df7880f1f22b9a844d7b1
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2024-09-11 11:47:03 -0500

    Content updates

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -37,17 +37,19 @@ export default function ImagePreview({ images }: { images: RichImage[] }) {
       <div className="main-image-container">
         <img src={selectedImage.src} alt={selectedImage.alt} />
       </div>
-      {previewedImages.length > 0 && <div className="image-previews">
-        { previewedImages.map((image) => (
-          <button key={image.id}>
-            <img
-              src={image.src}
-              alt={image.alt}
-              onClick={() => setSelectedAndResetTimeout(image.idx)}
-            />
-          </button>
-        ))}
-      </div>}
+      {previewedImages.length > 0 && (
+        <div className="image-previews">
+          {previewedImages.map((image) => (
+            <button key={image.id}>
+              <img
+                src={image.src}
+                alt={image.alt}
+                onClick={() => setSelectedAndResetTimeout(image.idx)}
+              />
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,10 +1,17 @@
 import '@/styles/Projects.css';
 import ProjectCard from '@/components/ProjectCard';
-import { useEffect } from 'react';
 import type { ProjectType } from '@/common/data';
 
-import horseback from '../assets/horseback.png';
-import IllinoisLogo from '../assets/illinoislogo.png';
+import AnkiAlgoLogo from '../assets/projects/ankialgo.png';
+import CeptronLogo from '../assets/projects/ceptron.png';
+import CeptronComplete from '@/assets/projects/ceptron/complete.png';
+import CeptronOpened from '@/assets/projects/ceptron/opened.png';
+import RembrandtLogo from '@/assets/projects/rembrandt.png';
+import RembrandtHome from '@/assets/projects/rembrandt/rembrandt home.jpg';
+import Polaroid from '@/assets/projects/rembrandt/polaroid.png';
+import DalleVsRembrandt from '@/assets/projects/rembrandt/dalle vs rembrandt.jpg';
+
+import ClubfinityLogo from '@/assets/projects/clubfinity/ClubfinityLogo.png';
 
 const projects: ProjectType[] = [
   {
@@ -24,24 +31,79 @@ const projects: ProjectType[] = [
       `I use the similiarity between vector embeddings of each question's optimal 
       solution to provide fine-grained comparisons of question strategies.`,
     ],
-    images: [
-      { id: '1', src: horseback.src, alt: 'horseback' },
-      { id: '2', src: IllinoisLogo.src, alt: 'horseback' },
-      { id: '3', src: horseback.src, alt: 'horseback' },
-      { id: '4', src: IllinoisLogo.src, alt: 'horseback' },
-      { id: '5', src: IllinoisLogo.src, alt: 'horseback' },
-    ],
+    images: [{ id: '1', src: AnkiAlgoLogo.src, alt: 'AnkiAlgo Logo' }],
     tech: ['TypeScript', 'Next.js', 'Postgres', 'Docker', 'Java', 'AWS'],
+  },
+  {
+    title: 'Rembrandt',
+    status: ['Completed', 'Shelved'],
+    link: 'https://devpost.com/software/rembrandt',
+    blurb: `Website for generating images in the style of a brand, given example images`,
+    details: [
+      'Rembrandt uses GPT Vision to analyze a set of images uploaded by a user for key branding aspects such as color scheme, general themes, and items.',
+      `Users can then input prompts for images, which Ceptron then uses alongside their themes via prompt engineering to generate images in the brand's style.`,
+      'Rembrandt is built in React and uses AWS Lambda for its backend.',
+    ],
+    images: [
+      { id: '1', src: RembrandtLogo.src, alt: 'Rembrandt Logo' },
+      {
+        id: '2',
+        src: RembrandtHome.src,
+        alt: 'The home page for Rembrandt',
+      },
+      {
+        id: '3',
+        src: Polaroid.src,
+        alt: 'A comparison of Dall-E and Rembrandt results for Polaroid',
+      },
+      {
+        id: '4',
+        src: DalleVsRembrandt.src,
+        alt: `A comparison of DallE and Rembrandt results for TikTok`,
+      },
+    ],
+    tech: [],
   },
   {
     title: 'Ceptron',
     status: ['Completed', 'Shelved'],
-    link: 'https://github.com/yimothysu/ceptron',
+    link: 'https://devpost.com/software/ceptron',
     blurb: `An AI assistant tool suite, in the style of MacOS's spotlight`,
-    details: ['I was making GPT wrappers before it was cool. 😎'],
-    images: [{ id: '1', src: horseback.src, alt: 'horseback'}],
+    details: [
+      'I was making GPT wrappers before it was cool. 😎',
+      'Ceptron provides an easy-to-use interface that brings all the power of AI to your fingertips.',
+      'At the time of building, Ceptron used GPT-3 for text completion and self-hosted Stable Diffusion for image generation.',
+      'This project was built for HackGT 9 in 2022. Special shoutout to my teammates and especially Jim Su, who helped me break \
+      through to a whole new understanding of the process of building software.',
+    ],
+    images: [
+      { id: '1', src: CeptronLogo.src, alt: 'Ceptron Logo' },
+      {
+        id: '2',
+        src: CeptronOpened.src,
+        alt: 'Ceptron, ready to be used',
+      },
+      {
+        id: '3',
+        src: CeptronComplete.src,
+        alt: `Ceptron's "complete" command`,
+      },
+    ],
     tech: [],
-  }
+  },
+  {
+    title: 'Clubfinity',
+    status: ['Shelved'],
+    link: 'https://gitlab.com/ufsec/clubfinity',
+    blurb: `An app for clubs to coordinate, collaborate, and keep their members in the loop.`,
+    details: [
+      'As the Lead Developer, I led 5 teams totaling 23 people developing a full stack mobile app for clubs built using React Native and Node.js',
+      `I developed features, fixed bugs, conducted code reviews, and implemented industry practices to mentor students`,
+      'Finally, I managed deployment and led a closed beta with clubs around campus.',
+    ],
+    images: [{ id: '1', src: ClubfinityLogo.src, alt: 'Clubfinity Logo' }],
+    tech: [],
+  },
 ];
 
 export default function Projects() {
