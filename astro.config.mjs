3228a5e5e02a7a391f5caeac253190ec700d8a86
Alex Rowe <alexrowe@meta.com>
2026-07-14 17:19:50 -0700

    Update Dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import { fileURLToPath } from 'node:url';
 
 // For some reason, removing this breaks the page's images object-fit
 import tailwind from '@astrojs/tailwind';
@@ -8,4 +9,11 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://alexfprowe.com',
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
 });
